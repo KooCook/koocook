4c0e694d6890fc8aa6e143141b135e155f035f95
T. Bunratta <tharathorn.bu@ku.th>
2019-10-30 17:50:21 +0700

    Reorder GCP deployment sequences

--- scripts/deb-auto-deploy.sh
+++ scripts/deb-auto-deploy.sh
@@ -6,8 +6,9 @@ gcloud auth activate-service-account --key-file gae_skey.json
 cd koocook-proxy/ || exit
 git pull
 cd ~/koocook-app  || exit
-gcloud app deploy koocook-proxy/app.yaml
 
 sudo docker image prune -f
 sudo docker-compose pull
 sudo docker-compose up -d
+
+gcloud app deploy koocook-proxy/app.yaml
