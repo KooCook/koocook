3db7f8b247b3f9fbd3124480bf4c718657ea1a63
T. Bunratta <tharathorn.bu@ku.th>
2019-10-30 15:53:56 +0700

    Configure GAE deployment for the reverse proxy server

--- scripts/deb-auto-deploy.sh
+++ scripts/deb-auto-deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cd ~/koocook-app  || exit
 
+
+gcloud auth activate-service-account --key-file gae_skey.json
+git submodule update --recursive --remote koocook-proxy/
+gcloud app deploy koocook-proxy/app.yaml
+
 sudo docker image prune -f
 sudo docker-compose pull
 sudo docker-compose up -d
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,7 @@
 BASEDIR=$(dirname "$0")
 cd "$BASEDIR" || exit
 cd .. || exit
+echo "$GAE_SKEY" > "$PWD/config/gae_skey.json"
+scp -o StrictHostKeyChecking=no "$PWD/config/gae_skey.json" "$SSH_USER@$SERVER_IP_ADR:~/koocook-app/"
 scp -o StrictHostKeyChecking=no "$PWD/docker-compose.yml" "$SSH_USER@$SERVER_IP_ADR:~/koocook-app/"
 ssh -o StrictHostKeyChecking=no "$SSH_USER@$SERVER_IP_ADR" 'bash -s' < "$BASEDIR/deb-auto-deploy.sh"
