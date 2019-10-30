ffefae0d5acdb0072dda5bdaa687293ae0ccecbb
T. Bunratta <tharathorn.bu@ku.th>
2019-10-30 16:10:44 +0700

    Another fix on the Deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@
 BASEDIR=$(dirname "$0")
 cd "$BASEDIR" || exit
 cd .. || exit
+mkdir "$PWD/config/"
 echo "$GAE_SKEY" > "$PWD/config/gae_skey.json"
 scp -o StrictHostKeyChecking=no "$PWD/config/gae_skey.json" "$SSH_USER@$SERVER_IP_ADR:~/koocook-app/"
 scp -o StrictHostKeyChecking=no "$PWD/docker-compose.yml" "$SSH_USER@$SERVER_IP_ADR:~/koocook-app/"
