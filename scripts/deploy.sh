0a5fcca85ae9b162012699202698c8fdaf5ad0e5
T. Bunratta <tharathorn.bu@ku.th>
2019-10-30 16:17:21 +0700

    Another fix on the Deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,6 @@
 BASEDIR=$(dirname "$0")
 cd "$BASEDIR" || exit
 cd .. || exit
-mkdir "$PWD/config/"
-echo "$GAE_SKEY" > "$PWD/config/gae_skey.json"
 scp -o StrictHostKeyChecking=no "$PWD/config/gae_skey.json" "$SSH_USER@$SERVER_IP_ADR:~/koocook-app/"
 scp -o StrictHostKeyChecking=no "$PWD/docker-compose.yml" "$SSH_USER@$SERVER_IP_ADR:~/koocook-app/"
 ssh -o StrictHostKeyChecking=no "$SSH_USER@$SERVER_IP_ADR" 'bash -s' < "$BASEDIR/deb-auto-deploy.sh"
