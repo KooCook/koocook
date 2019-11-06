bf258139e99f8686bc3d93f9080d0ce330b580e9
T. Bunratta <tharathorn.bu@ku.th>
2019-11-06 11:59:33 +0700

    Modify the deployment script for Django migrations

--- scripts/deb-auto-deploy.sh
+++ scripts/deb-auto-deploy.sh
@@ -10,5 +10,6 @@ cd ~/koocook-app  || exit
 sudo docker image prune -f
 sudo docker-compose pull
 sudo docker-compose up -d
+sudo docker exec -it koocook-dj python manage.py migrate
 
 gcloud app deploy koocook-proxy/app.yaml
