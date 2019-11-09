1468cca9212554c2f971191955afb208f29b9337
T. Bunratta <tharathorn.bu@ku.th>
2019-11-10 03:00:36 +0700

    Attach the image volume to the 'static' directory

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,5 @@ BASEDIR=$(dirname "$0")
 cd "$BASEDIR" || exit
 cd .. || exit
 scp -o StrictHostKeyChecking=no "$PWD/config/gae_skey.json" "$SSH_USER@$SERVER_IP_ADR:~/koocook-app/"
-scp -o StrictHostKeyChecking=no "$PWD/docker-compose.yml" "$SSH_USER@$SERVER_IP_ADR:~/koocook-app/"
+#scp -o StrictHostKeyChecking=no "$PWD/docker-compose.yml" "$SSH_USER@$SERVER_IP_ADR:~/koocook-app/"
 ssh -o StrictHostKeyChecking=no "$SSH_USER@$SERVER_IP_ADR" 'bash -s' < "$BASEDIR/deb-auto-deploy.sh"
