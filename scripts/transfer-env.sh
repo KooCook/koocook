7045429fb5ce9c803d066f6a40a899727ec2d5ad
T. Bunratta <tharathorn.bu@ku.th>
2019-11-16 00:30:55 +0700

    Purge some scripts from the repo

--- scripts/transfer-env.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-scp -o StrictHostKeyChecking=no -i ~/.ssh/gce_koocook_2 "$PWD/config/db.env" soscamp8@35.197.101.143:~/koocook-app/config/
-scp -o StrictHostKeyChecking=no -i ~/.ssh/gce_koocook_2 "$PWD/config/dj.env" soscamp8@35.197.101.143:~/koocook-app/config/
