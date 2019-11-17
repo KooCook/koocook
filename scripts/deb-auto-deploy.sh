a20e94a78807a3f796789dd80f6e7f59749fda44
T. Bunratta <tharathorn.bu@ku.th>
2019-11-18 00:11:40 +0700

    Minor fix

--- scripts/deb-auto-deploy.sh
+++ scripts/deb-auto-deploy.sh
@@ -7,7 +7,7 @@ git submodule sync
 git submodule update --recursive --remote
 
 sudo docker image prune -f
-sudo docker-compose pull
+sudo docker-compose build
 sudo docker-compose up -d
 sudo docker exec koocook-dj python manage.py makemigrations
 sudo docker exec koocook-dj python manage.py migrate
